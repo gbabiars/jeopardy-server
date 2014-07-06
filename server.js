5cfac0332368362c492cb6cfb53fd419d020ee3d
Greg Babiars <gbabiars@gmail.com>
2014-07-05 21:56:12 -0700

    Added back mongo

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var Hapi = require('hapi');
-//    MongoClient = require('mongodb').MongoClient;
+    MongoClient = require('mongodb').MongoClient;
 
 var port = parseInt(process.env.PORT) || 8000;
 var host = '0.0.0.0';
 
 var server = Hapi.createServer(host, port, { cors: true });
 
-//var connectionString = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/jeopardy';
-//
-//MongoClient.connect(connectionString, function(err, db) {
-//    if(err) {
-//        throw err;
-//    }
-//
-//    server.app.db = db;
-//    console.log('Connected to Mongo');
-//});
+var connectionString = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/jeopardy';
+
+MongoClient.connect(connectionString, function(err, db) {
+    if(err) {
+        throw err;
+    }
+
+    server.app.db = db;
+    console.log('Connected to Mongo');
+});
 
 server.route(require('./routes'));
 
