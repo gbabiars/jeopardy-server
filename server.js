f2a562f0ac444b91896055c5a19d366a25c35dd8
Greg Babiars <gbabiars@gmail.com>
2014-07-05 20:43:20 -0700

    Trying without mongo

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var Hapi = require('Hapi'),
-    MongoClient = require('mongodb').MongoClient;
+var Hapi = require('Hapi');
+//    MongoClient = require('mongodb').MongoClient;
 
 var port = parseInt(process.env.PORT) || 8000;
 var host = process.env.HOST || 'localhost';
 
 var server = Hapi.createServer(host, port, { cors: true });
 
-var connectionString = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/jeopardy';
-
-MongoClient.connect(connectionString, function(err, db) {
-    if(err) {
-        throw err;
-    }
-
-    server.app.db = db;
-    console.log('Connected to Mongo');
-});
+//var connectionString = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/jeopardy';
+//
+//MongoClient.connect(connectionString, function(err, db) {
+//    if(err) {
+//        throw err;
+//    }
+//
+//    server.app.db = db;
+//    console.log('Connected to Mongo');
+//});
 
 server.route(require('./routes'));
 
